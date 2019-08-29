1221ae9d6991528b0ad31050ac62ce41881fda47
Angel Herrera Medina <angelherrerax7@gmail.com>
2019-08-29 16:57:18 -0500

    Se fixeo el spinner

--- PaHis/PaHis/Extensions/UIViewControllerExtensions.swift
+++ PaHis/PaHis/Extensions/UIViewControllerExtensions.swift
@@ -10,18 +10,33 @@ import UIKit
 
 extension UIViewController {
     class func displaySpinner(onView : UIView) -> UIView {
-        let spinnerView = UIView.init(frame: onView.bounds)
+        let spinnerView = UIView()
         spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
         let activityIndicator = UIActivityIndicatorView.init(style: .whiteLarge)
-//        activityIndicator.color = UIColor(rgb: 0xF5391C)
+        //        activityIndicator.color = UIColor(rgb: 0xF5391C)
         activityIndicator.startAnimating()
-        activityIndicator.center = spinnerView.center
+        spinnerView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        spinnerView.translatesAutoresizingMaskIntoConstraints = false
         
-        DispatchQueue.main.async {
-            spinnerView.addSubview(activityIndicator)
+        guard let window = UIApplication.shared.keyWindow else {
             onView.addSubview(spinnerView)
+            spinnerView.topAnchor.constraint(equalTo: onView.topAnchor, constant: 0).isActive = true
+            spinnerView.trailingAnchor.constraint(equalTo: onView.trailingAnchor, constant: 0).isActive = true
+            spinnerView.leadingAnchor.constraint(equalTo: onView.leadingAnchor, constant: 0).isActive = true
+            spinnerView.bottomAnchor.constraint(equalTo: onView.bottomAnchor, constant: 0).isActive = true
+            activityIndicator.centerXAnchor.constraint(equalTo: spinnerView.centerXAnchor, constant: 0).isActive = true
+            activityIndicator.centerYAnchor.constraint(equalTo: spinnerView.centerYAnchor, constant: 0).isActive = true
+            return spinnerView
         }
         
+        window.addSubview(spinnerView)
+        spinnerView.topAnchor.constraint(equalTo: window.superview?.topAnchor ?? window.topAnchor, constant: 0).isActive = true
+        spinnerView.trailingAnchor.constraint(equalTo: window.superview?.trailingAnchor ?? window.trailingAnchor, constant: 0).isActive = true
+        spinnerView.leadingAnchor.constraint(equalTo: window.superview?.leadingAnchor ?? window.leadingAnchor, constant: 0).isActive = true
+        spinnerView.bottomAnchor.constraint(equalTo: window.superview?.bottomAnchor ?? window.bottomAnchor, constant: 0).isActive = true
+        activityIndicator.centerXAnchor.constraint(equalTo: spinnerView.centerXAnchor, constant: 0).isActive = true
+        activityIndicator.centerYAnchor.constraint(equalTo: spinnerView.centerYAnchor, constant: 0).isActive = true
         return spinnerView
     }
     
