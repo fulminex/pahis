26af8bc30b5d5557d061482c6ab91855c38c16a5
Leonardo Luna <lplpalomino@gmail.com>
2019-10-14 13:50:40 -0500

    observacion 13

--- PaHis/PaHis/Scenes/FiltersScene/FiltersViewController.swift
+++ PaHis/PaHis/Scenes/FiltersScene/FiltersViewController.swift
@@ -29,47 +29,69 @@ class FiltersViewController: UIViewController {
     var isProvincia = false
     var isDistrito = false
     
-    var selectedCategory: CategoryPahis?
-    var selectedDepartment: Ubigeo?
-    var selectedProvincia: Ubigeo?
-    var selectedDistrito: Ubigeo?
+    static var selectedCategory: CategoryPahis?
+    static var selectedDepartment: Ubigeo?
+    static var selectedProvincia: Ubigeo?
+    static var selectedDistrito: Ubigeo?
     
     var categories: [CategoryPahis]!
     var departments: [Ubigeo]?
     var provincias: [Ubigeo]?
     var distritos: [Ubigeo]?
     
+    static var categoryValue: Int = 0
+    static var departmentValue: Int = 0
+    static var provinciaValue: Int = 0
+    static var distritoValue: Int = 0
+    
     var delegate: FilterPopUpDelegate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupFileds()
         fetchDepartments()
+    }
+    
+    func setupFileds() {
+        if let category = FiltersViewController.selectedCategory {
+            categoryButton.setTitle(category.name, for: .normal)
+        }
+        if let department = FiltersViewController.selectedDepartment {
+            departmentButton.setTitle(department.nombre, for: .normal)
+        }
+        if let provincia = FiltersViewController.selectedProvincia {
+            provinciaButton.setTitle(provincia.nombre, for: .normal)
+        }
+        if let distrito = FiltersViewController.selectedDistrito {
+            distritoButton.setTitle(distrito.nombre, for: .normal)
+        }
+        
     }
     
     @IBAction func applyButtonPressed(_ sender: UIButton) {
         var codUbigeo: String? = nil
-        if selectedDepartment != nil {
-            codUbigeo = String(String(selectedDepartment!.codUbigeo!).prefix(2))
-            if selectedProvincia != nil {
-                codUbigeo = String(String(selectedProvincia!.codUbigeo!).prefix(4))
-                if selectedDistrito != nil {
-                    codUbigeo = String(selectedDistrito!.codUbigeo!)
+        if FiltersViewController.selectedDepartment != nil {
+            codUbigeo = String(String(FiltersViewController.selectedDepartment!.codUbigeo!).prefix(2))
+            if FiltersViewController.selectedProvincia != nil {
+                codUbigeo = String(String(FiltersViewController.selectedProvincia!.codUbigeo!).prefix(4))
+                if FiltersViewController.selectedDistrito != nil {
+                    codUbigeo = String(FiltersViewController.selectedDistrito!.codUbigeo!)
                 }
             }
         }
-        delegate.getFilter(filter: Filter(categoryID: selectedCategory != nil ? String(selectedCategory!.id!) : nil , codUbigeo: codUbigeo))
+        delegate.getFilter(filter: Filter(categoryID: FiltersViewController.selectedCategory != nil ? String(FiltersViewController.selectedCategory!.id!) : nil , codUbigeo: codUbigeo))
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func categoryButtonPressed(_ sender: UIButton) {
         isCategory = true
-        openPicker(items: ["Todas"] + categories.map({ $0.name! }))
+        openPicker(items: ["Todas"] + categories.map({ $0.name! }), index: FiltersViewController.categoryValue)
     }
     
     @IBAction func departmentButtonPressedç(_ sender: UIButton) {
         guard departments != nil else { return }
         isDepartment = true
-        openPicker(items: ["Todos"] + departments!.map({ $0.nombre! }))
+        openPicker(items: ["Todos"] + departments!.map({ $0.nombre! }), index: FiltersViewController.departmentValue)
     }
     
     @IBAction func provinciaButtonPressed(_ sender: UIButton) {
@@ -80,7 +102,7 @@ class FiltersViewController: UIViewController {
             return
         }
         isProvincia = true
-        openPicker(items: ["Todas"] + provincias!.map({ $0.nombre! }))
+        openPicker(items: ["Todas"] + provincias!.map({ $0.nombre! }), index: FiltersViewController.provinciaValue)
     }
     
     @IBAction func distritoButtonPressed(_ sender: UIButton) {
@@ -91,7 +113,7 @@ class FiltersViewController: UIViewController {
             return
         }
         isDistrito = true
-        openPicker(items: ["Todos"] + distritos!.map({ $0.nombre! }))
+        openPicker(items: ["Todos"] + distritos!.map({ $0.nombre! }), index: FiltersViewController.distritoValue)
     }
     
     func fetchDepartments() {
@@ -109,7 +131,7 @@ class FiltersViewController: UIViewController {
     
     func fetchProvincias() {
         let spinner = UIViewController.displaySpinner(onView: self.view)
-        let id = String(selectedDepartment!.codUbigeo!).prefix(2)
+        let id = String(FiltersViewController.selectedDepartment!.codUbigeo!).prefix(2)
         NetworkManager.shared.getProvincias(departmentID: String(id)) { (result) in
             UIViewController.removeSpinner(spinner: spinner)
             switch result {
@@ -123,7 +145,7 @@ class FiltersViewController: UIViewController {
     
     func fetchDistritos() {
         let spinner = UIViewController.displaySpinner(onView: self.view)
-        let id = String(selectedProvincia!.codUbigeo!).prefix(4)
+        let id = String(FiltersViewController.selectedProvincia!.codUbigeo!).prefix(4)
         NetworkManager.shared.getDistritos(id: String(id)) { (result) in
             UIViewController.removeSpinner(spinner: spinner)
             switch result {
@@ -135,10 +157,11 @@ class FiltersViewController: UIViewController {
         }
     }
     
-    func openPicker(items: [String]) {
+    func openPicker(items: [String],index: Int) {
         let sb = UIStoryboard(name: "Picker", bundle: nil)
         let vc = sb.instantiateInitialViewController() as! PickerViewController
         vc.items = items
+        vc.index = index
         vc.delegate = self
         vc.modalTransitionStyle = .crossDissolve
         vc.modalPresentationStyle = .overCurrentContext
@@ -148,56 +171,60 @@ class FiltersViewController: UIViewController {
 }
 
 extension FiltersViewController: PopUpPickerViewDelegate {
-    func getItemSelected(value: String) {
+    func getItemSelected(value: String, index: Int) {
         if isCategory {
             isCategory = false
+            FiltersViewController.categoryValue = index
             categoryButton.setTitle(value, for: .normal)
             guard value != "Todas" else {
-                selectedCategory = nil
+                FiltersViewController.selectedCategory = nil
                 return
             }
-            selectedCategory = categories?.filter({ $0.name == value }).first!
+            FiltersViewController.selectedCategory = categories?.filter({ $0.name == value }).first!
         }
         if isDepartment {
+            FiltersViewController.departmentValue = index
             isDepartment = false
             departmentButton.setTitle(value, for: .normal)
             guard value != "Todos" else {
-                selectedDepartment = nil
-                selectedProvincia = nil
-                selectedDistrito = nil
+                FiltersViewController.selectedDepartment = nil
+                FiltersViewController.selectedProvincia = nil
+                FiltersViewController.selectedDistrito = nil
                 provinciaButton.setTitle("Todas", for: .normal)
                 distritoButton.setTitle("Todos", for: .normal)
                 return
             }
-            selectedDepartment = departments?.filter({ $0.nombre == value }).first!
-            selectedProvincia = nil
-            selectedDistrito = nil
+            FiltersViewController.selectedDepartment = departments?.filter({ $0.nombre == value }).first!
+            FiltersViewController.selectedProvincia = nil
+            FiltersViewController.selectedDistrito = nil
             provinciaButton.setTitle("Todas", for: .normal)
             distritoButton.setTitle("Todos", for: .normal)
             fetchProvincias()
         }
         if isProvincia {
+            FiltersViewController.provinciaValue = index
             isProvincia = false
             provinciaButton.setTitle(value, for: .normal)
             guard value != "Todas" else {
-                selectedProvincia = nil
-                selectedDistrito = nil
+                FiltersViewController.selectedProvincia = nil
+                FiltersViewController.selectedDistrito = nil
                 distritoButton.setTitle("Todos", for: .normal)
                 return
             }
-            selectedProvincia = provincias?.filter({ $0.nombre == value }).first!
-            selectedDistrito = nil
+            FiltersViewController.selectedProvincia = provincias?.filter({ $0.nombre == value }).first!
+            FiltersViewController.selectedDistrito = nil
             distritoButton.setTitle("Todos", for: .normal)
             fetchDistritos()
         }
         if isDistrito {
+            FiltersViewController.provinciaValue = index
             isDistrito = false
             distritoButton.setTitle(value, for: .normal)
             guard value != "Todos" else {
-                selectedDistrito = nil
+                FiltersViewController.selectedDistrito = nil
                 return
             }
-            selectedDistrito = distritos?.filter({ $0.nombre == value }).first!
+            FiltersViewController.selectedDistrito = distritos?.filter({ $0.nombre == value }).first!
         }
     }
     
